be8b6ef772e7005ca96c79fec4ab228e76d050fb
Henrique Ribeiro <henrichrb@gmail.com>
2023-08-28 14:38:31 -0300

    add: sistema basico de calculadora

---
+++ aula 6 -aprofundamento em funcoes/tiposDeFuncao.js
@@ -0,0 +1,23 @@
+// Tipos de funções
+
+// Declarativa
+/*function funcao() {
+    console.log('Sou uma mensagem de uma função declarativa');
+}
+
+funcao();
+
+// Expressão de função
+// Com nomeação
+var funcao = function funcao() {
+    console.log('Sou uma mensagem de função de expressão!');
+}
+
+funcao();
+
+// Arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function!');
+}
+
+funcao()*/
